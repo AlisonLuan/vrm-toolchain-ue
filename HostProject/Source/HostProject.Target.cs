69f18f0d9b33ceec77e2719e106c9bf0f7161f36
Alison Luan Nunes de Souza <alisonluan@hotmail.com>
2026-01-16 23:27:54 +0100

    Update build settings and engine version to 5.7
    Updated HostProject.Target.cs to use BuildSettingsVersion.V6, set IncludeOrderVersion to Unreal5_7, and CppStandard to Cpp20. Also updated VrmToolchain.uplugin to require Unreal Engine 5.7.0. These changes align the project and plugin with the latest Unreal Engine standards.

--- HostProject/Source/HostProject.Target.cs
+++ HostProject/Source/HostProject.Target.cs
@@ -5,10 +5,14 @@ using System.Collections.Generic;
 
 public class HostProjectTarget : TargetRules
 {
-	public HostProjectTarget(TargetInfo Target) : base(Target)
-	{
-		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		ExtraModuleNames.AddRange(new string[] { "HostProject" });
-	}
+    public HostProjectTarget(TargetInfo Target) : base(Target)
+    {
+        Type = TargetType.Game;
+
+        DefaultBuildSettings = BuildSettingsVersion.V6;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+        CppStandard = CppStandardVersion.Cpp20;
+
+        ExtraModuleNames.AddRange(new string[] { "HostProject" });
+    }
 }
